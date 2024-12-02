b8d3e900d7dec403f47e2e7f2f223ed4bfcf0613
andreasheinz <andreas.heinz@cec.valantic.com>
2024-12-02 10:25:40 +0100

    first commit

--- src/Controller/BananaController.php
+++ src/Controller/BananaController.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class BananaController
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BananaController extends AbstractController
 {
 
+    #[Route('/')]
+    public function bananaController(): Response
+    {
+        return $this->render('main/banana.html.twig');
+    }
 }
