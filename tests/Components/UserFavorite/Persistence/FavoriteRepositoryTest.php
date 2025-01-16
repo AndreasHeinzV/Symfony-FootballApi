b3030a90710e7248d0830305bf93583c98038608
andreasheinz <andreas.heinz@cec.valantic.com>
2025-01-16 09:49:02 +0100

    implemented tests for FavoriteRepository

--- tests/Components/UserFavorite/Persistence/FavoriteRepositoryTest.php
+++ tests/Components/UserFavorite/Persistence/FavoriteRepositoryTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests\Components\UserFavorite\Persistence;
 
+use App\Components\UserFavorite\Persistence\FavoriteDTO;
 use App\Components\UserFavorite\Persistence\FavoriteRepository;
+use App\Entity\Favorite;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class FavoriteRepositoryTest extends WebTestCase
@@ -20,10 +22,80 @@ class FavoriteRepositoryTest extends WebTestCase
         $this->repository = $container->get(FavoriteRepository::class);
     }
 
-    public function testGetAllFavorites(): void
+    public function testGetAllFavoritesFromFirstUser(): void
     {
         $favorites = $this->repository->getUserFavorites(1);
         self::assertNotEmpty($favorites);
-      //  dd($favorites);
+        self::assertInstanceOf(FavoriteDTO::class, $favorites[0]);
+        self::assertCount(3, $favorites);
     }
+
+    public function testGetUserFavoriteFromUserWithoutFavorites(): void
+    {
+        $favorites = $this->repository->getUserFavorites(2);
+        self::assertEmpty($favorites);
+    }
+
+    public function testGetUserFavoritesWithWrongUserId(): void
+    {
+        $favorites = $this->repository->getUserFavorites(4);
+        self::assertEmpty($favorites);
+    }
+
+    public function testGetUserFavoriteWithTeamIdOne(): void
+    {
+        $favorite = $this->repository->getUserFavoriteByTeamId(1, 1);
+        self::assertNotEmpty($favorite);
+        self::assertInstanceOf(Favorite::class, $favorite);
+        self::assertSame(1, $favorite->getTeamId());
+        self::assertSame('team1', $favorite->getTeamName());
+    }
+
+    public function testGetUserFavoriteWithWrongTeamId(): void
+    {
+        $favorite = $this->repository->getUserFavoriteByTeamId(2, 6);
+        self::assertNull($favorite);
+    }
+
+    public function testGetUserFavoritesFirstFavoritePosition(): void
+    {
+        $favoritePosition = $this->repository->getUserFavoritesFirstPosition(1);
+
+        self::assertSame(1, $favoritePosition);
+    }
+
+    public function testGetUserFavoritesLastFavoritePosition(): void
+    {
+        $favoritePosition = $this->repository->getUserFavoritesLastPosition(1);
+
+        self::assertSame(3, $favoritePosition);
+    }
+
+    public function testGetUserFavoriteFirstPositionForEmptyUsers(): void
+    {
+        $favoritePosition = $this->repository->getUserFavoritesFirstPosition(2);
+
+        self::assertFalse($favoritePosition);
+    }
+
+    public function testGetUserFavoriteLastPositionForEmptyUsers(): void
+    {
+        $favoritePosition = $this->repository->getUserFavoritesLastPosition(2);
+        self::assertFalse($favoritePosition);
+    }
+
+    public function testGetUserFavoriteEntityWithPosition(): void
+    {
+        $favoriteEntity = $this->repository->getUserFavoriteEntityByPosition(1, 2);
+        self::assertInstanceOf(Favorite::class, $favoriteEntity);
+        self::assertSame(2, $favoriteEntity->getFavoritePosition());
+        self::assertSame('team2', $favoriteEntity->getTeamName());
+    }
+
+    public function testGetUserFavoriteEntityWithWrongPosition(): void
+    {
+        $favoriteEntity = $this->repository->getUserFavoriteEntityByPosition(2, 6);
+        self::assertNull($favoriteEntity);
+    }
+
 }
